051a85ab556c778bf167346fecfeb7f762ae1f2d
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2017-02-10 12:38:37 +0000

    Change CAS.UA.IServerConfiguration add-in installation directory - Moving the ModelContainer to ModelDesigner solution folder. Issue #: 4779

---
+++ VS/trunk/CommServer.UA.ModelDesigner/ModelDesigner.ModelsContainer.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿//<summary>
+//  Title   : Assembly info for: CAS.CommServer.UA.ConfigurationEditor.ModelsContainer.UnitTests
+//  System  : Microsoft Visual C# .NET
+//  $LastChangedDate$
+//  $Rev$
+//  $LastChangedBy$
+//  $URL$
+//  $Id$
+//
+//  Copyright (c) 2017 CAS LODZ POLAND
+//  +48 (42) 686 25 47
+//  techsupp@cas.eu
+//  www.cas.eu
+//</summary>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CAS.CommServer.UA.ConfigurationEditor.ModelsContainer.UnitTests")]
+[assembly: AssemblyDescription("UA ConfigurationEditor Models Container Unit Tests")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("CAS CommServer Family")]
+[assembly: AssemblyCopyright("Copyright (c) 2017 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("CommServer")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid( "7f6b3ca8-1d1c-41e2-85d9-938cd835034d" )]
+[assembly: AssemblyVersion("3.20.4.*")]
+[assembly: AssemblyFileVersion("3.20.4")]
+
