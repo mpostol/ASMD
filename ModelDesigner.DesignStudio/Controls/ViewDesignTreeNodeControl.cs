9025fd30e42f06738912a02a4fac2582f14ca887
mpostol <mpostol@cas.eu>
2019-11-03 20:32:53 +0100

    Refactoring application architecture - remove recursion #6
    - Removing TreeNode controls from the CAS.UA.Model.Designer.Wrappers to CAS.UA.Model.Designer.Controls
    - Created ViewDesignTreeNodeControl
    - Work in progress

---
+++ ModelDesigner.DesignStudio/Controls/ViewDesignTreeNodeControl.cs
@@ -0,0 +1,17 @@
+﻿//___________________________________________________________________________________
+//
+//  Copyright (C) 2019, Mariusz Postol LODZ POLAND.
+//
+//___________________________________________________________________________________
+
+using CAS.UA.Model.Designer.Wrappers;
+
+namespace CAS.UA.Model.Designer.Controls
+{
+  internal class ViewDesignTreeNodeControl : CAS.UA.Model.Designer.Controls.NodeDesignTreeNodeControl<ViewDesign, Wrappers4ProperyGrid.ViewDesign, Opc.Ua.ModelCompiler.ViewDesign>
+  {
+    public ViewDesignTreeNodeControl(ViewDesign parent)
+      : base(parent)
+    { }
+  }
+}
--- ModelDesigner.DesignStudio/Wrappers/ViewDesign.cs
+++ ModelDesigner.DesignStudio/Wrappers/ViewDesign.cs
@@ -1,41 +1,30 @@
-﻿//<summary>
-//  Title   : View Design wrapper
-//  System  : Microsoft Visual C# .NET 2008
-//  $LastChangedDate$
-//  $Rev$
-//  $LastChangedBy$
-//  $URL$
-//  $Id$
+﻿//___________________________________________________________________________________
 //
+//  Copyright (C) 2019, Mariusz Postol LODZ POLAND.
 //
-//  Copyright (C)2008, CAS LODZ POLAND.
-//  TEL: +48 (42) 686 25 47
-//  mailto://techsupp@cas.eu
-//  http://www.cas.eu
-//</summary>
+//___________________________________________________________________________________
 
 using CAS.UA.Model.Designer.Properties;
 
 namespace CAS.UA.Model.Designer.Wrappers
 {
-  internal partial class ViewDesign: NodeDesign<Wrappers4ProperyGrid.ViewDesign, Opc.Ua.ModelCompiler.ViewDesign>
+  internal partial class ViewDesign : NodeDesign<Wrappers4ProperyGrid.ViewDesign, Opc.Ua.ModelCompiler.ViewDesign>
   {
-    #region creators
+    
+    #region constructors
     public ViewDesign()
-      : base( new Designer.Wrappers4ProperyGrid.ViewDesign( new Opc.Ua.ModelCompiler.ViewDesign() ) )
-    { }
-    public ViewDesign( Opc.Ua.ModelCompiler.ViewDesign node )
-      : base( new Wrappers4ProperyGrid.ViewDesign( node ), node )
-    { }
+      : base(new Designer.Wrappers4ProperyGrid.ViewDesign(new Opc.Ua.ModelCompiler.ViewDesign())) { }
+    public ViewDesign(Opc.Ua.ModelCompiler.ViewDesign node)
+      : base(new Wrappers4ProperyGrid.ViewDesign(node), node) { }
     #endregion
 
     #region private
-    private class TreeNode: CAS.UA.Model.Designer.Controls.NodeDesignTreeNodeControl<ViewDesign, Wrappers4ProperyGrid.ViewDesign, Opc.Ua.ModelCompiler.ViewDesign>
-    {
-      public TreeNode( ViewDesign parent )
-        : base( parent )
-      { }
-    }
+    //private class ViewDesignTreeNodeControl : CAS.UA.Model.Designer.Controls.NodeDesignTreeNodeControl<ViewDesign, Wrappers4ProperyGrid.ViewDesign, Opc.Ua.ModelCompiler.ViewDesign>
+    //{
+    //  public ViewDesignTreeNodeControl( ViewDesign parent )
+    //    : base( parent )
+    //  { }
+    //}
     #endregion
 
     #region public
@@ -43,18 +32,12 @@ namespace CAS.UA.Model.Designer.Wrappers
     /// Gets the name of the help topic.
     /// </summary>
     /// <value>The name of the help topic.</value>
-    public override string HelpTopicName
-    {
-      get { return Resources.NodeClasses_Objects_View; }
-    }
+    public override string HelpTopicName => Resources.NodeClasses_Objects_View;
     /// <summary>
     /// Gets the node class.
     /// </summary>
     /// <value>The node class.</value>
-    public override NodeClassesEnum NodeClass
-    {
-      get { return NodeClassesEnum.View; }
-    }
+    public override NodeClassesEnum NodeClass => NodeClassesEnum.View;
     /// <summary>
     /// Gets the tree node and all children.
     /// </summary>
@@ -63,7 +46,7 @@ namespace CAS.UA.Model.Designer.Wrappers
     /// </returns>
     public override BaseDictionaryTreeNode GetTreeNode()
     {
-      return new TreeNode( this );
+      return new TreeNode(this);
     }
     #endregion
   }
