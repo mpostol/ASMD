e8555582bd5f07d97e87d687c8960b31f558b265
mpostol <mpostol@cas.eu>
2020-02-12 16:56:16 +0100

    UAOOI.ASMD.GUIAbstractions must be created #103
    - added NuGetMetadata.nuspec and published the package

--- ASMD.GUIAbstractions/ICanBeAccepted.cs
+++ ASMD.GUIAbstractions/ICanBeAccepted.cs
@@ -5,20 +5,21 @@
 //  To be in touch join the community at GITTER: https://gitter.im/mpostol/OPC-UA-OOI
 //___________________________________________________________________________________
 
-namespace UAOOI.Windows.GUIAbstractions
+namespace UAOOI.ASMD.GUIAbstractions
 {
 
   /// <summary>
   /// This interface is used by internal component to inform parent window that it is possible to enable accept button.
   /// </summary>
-  [System.Obsolete("Replace it by ICommand")]
   public interface ICanBeAccepted
   {
+
     /// <summary>
     /// It sets the information if accept button in the parent dialog window can be accepted
     /// </summary>
     /// <param name="pOKState">true if it could be accepted</param>
-    void CanBeAccepted( bool pOKState );
+    //TODO Move all GUI interfaces to UAOOI.ASMD.GUIAbstractions #104
+    void CanBeAccepted(bool pOKState);
 
   }
 }
