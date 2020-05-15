ec1b4da98cb8f9bc344bebb66f8c704219d7db38
mpostol <mpostol@cas.eu>
2020-05-15 14:24:15 +0200

    ModelDesigne.MeshDiagram3D - needs refactoring #116
    - updated the AssemblyInfo
    - UT :+1:
    - fixes #116

--- ModelDesigner.MeshDiagram3D/Properties/AssemblyInfo.cs
+++ ModelDesigner.MeshDiagram3D/Properties/AssemblyInfo.cs
@@ -1,56 +1,22 @@
-﻿//---------------------------------------------------------------------------
-// This file is part of the following project:
-// Mesh Diagram 3D
-// Author:  Maciej Zbrzezny
-// 
-// This source is subject to the Microsoft Public License (Ms-PL)
-// See http://meshdiagram3d.codeplex.com/license
+﻿//___________________________________________________________________________________
 //
-// For more information, see:
-// http://meshdiagram3d.codeplex.com/
+//  Copyright (C) 2020, Mariusz Postol LODZ POLAND.
 //
-//---------------------------------------------------------------------------
+//___________________________________________________________________________________
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("MeshDiagram3D")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CAS.CommServer.UA.ModelDesigner.MeshDiagram3D")]
+[assembly: AssemblyDescription("Address Space Model Designer MeshDiagram3D library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct( "MeshDiagram3D" )]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Mariusz Postol")]
+[assembly: AssemblyProduct("Object-Oriented Internet Address Space Model Designer")]
+[assembly: AssemblyCopyright("Copyright (c) 2020 Mariusz Postol")]
+[assembly: AssemblyTrademark("OOI ASMD")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: Guid("45D894AC-F150-4C63-BCAE-7D4F878F1D74")]
+[assembly: AssemblyVersion("4.2.0.*")]
+[assembly: AssemblyFileVersion("4.2.0")]
 
-//In order to begin building localizable applications, set 
-//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
-//inside a <PropertyGroup>.  For example, if you are using US english
-//in your source files, set the <UICulture> to en-US.  Then uncomment
-//the NeutralResourceLanguage attribute below.  Update the "en-US" in
-//the line below to match the UICulture setting in the project file.
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
