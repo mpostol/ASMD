663959fcf2652c17e2626d39f6305b41f8ca931f
mpostol <mpostol@cas.eu>
2020-06-06 21:33:55 +0200

    Update the relative path calculator using current .NET functionality #132
    - reuse the functionality in other projects
    - fixes #132

--- ModelDesigner.DesignStudio/Wrappers/SolutionTreeNode.cs
+++ ModelDesigner.DesignStudio/Wrappers/SolutionTreeNode.cs
@@ -6,8 +6,8 @@
 
 using CAS.CommServer.UA.Common;
 using CAS.CommServer.UA.ModelDesigner.Configuration;
+using CAS.CommServer.UA.ModelDesigner.Configuration.IO;
 using CAS.CommServer.UA.ModelDesigner.Configuration.UserInterface;
-using CAS.Lib.CodeProtect;
 using CAS.UA.IServerConfiguration;
 using CAS.UA.Model.Designer.IO;
 using CAS.UA.Model.Designer.Properties;
