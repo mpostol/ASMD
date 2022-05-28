c577f82e206642e4f7e0f11ddb34db7b7f0c9a8e
MPostol <mpostol@cas.eu>
2022-05-28 21:27:58 +0200

    Update the model compiler plug-in #198
    -Added project pc.Ua.ModelCompiler\ModelCompiler.csproj

--- ModelDesigner.DesignStudio/IO/ProjectConfigurationManagement.cs
+++ ModelDesigner.DesignStudio/IO/ProjectConfigurationManagement.cs
@@ -1,6 +1,6 @@
 ﻿//__________________________________________________________________________________________________
 //
-//  Copyright (C) 2021, Mariusz Postol LODZ POLAND.
+//  Copyright (C) 2022, Mariusz Postol LODZ POLAND.
 //
 //  To be in touch join the community at GitHub: https://github.com/mpostol/OPC-UA-OOI/discussions
 //__________________________________________________________________________________________________
