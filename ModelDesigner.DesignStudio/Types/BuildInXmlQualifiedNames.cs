2cbcd4a5ab759beba9757e5d9829a4973f3a8d39
MPostol <mpostol@cas.eu>
2022-05-29 21:13:43 +0200

    Update the model compiler plug-in #198
    - removed dependency on "CAS.CommServer.UA.ModelCompiler.Command" version="1.11.335.10"
    - added dependency on "OOI.ModelCompiler" version="3.1.0-rc" and all it depends on
    - build works

--- ModelDesigner.DesignStudio/Types/BuildInXmlQualifiedNames.cs
+++ ModelDesigner.DesignStudio/Types/BuildInXmlQualifiedNames.cs
@@ -1,11 +1,12 @@
-﻿//___________________________________________________________________________________
+﻿//__________________________________________________________________________________________________
 //
-//  Copyright (C) 2021, Mariusz Postol LODZ POLAND.
+//  Copyright (C) 2022, Mariusz Postol LODZ POLAND.
 //
-//  To be in touch join the community at GITTER: https://gitter.im/mpostol/OPC-UA-OOI
-//___________________________________________________________________________________
+//  To be in touch join the community at GitHub: https://github.com/mpostol/OPC-UA-OOI/discussions
+//__________________________________________________________________________________________________
 
 using System.Xml;
+using UAOOI.SemanticData.InformationModelFactory.UAConstants;
 using OpcUaModelCompiler = UAOOI.SemanticData.UAModelDesignExport.XML;
 
 namespace CAS.UA.Model.Designer.Types
@@ -15,42 +16,30 @@ namespace CAS.UA.Model.Designer.Types
   /// </summary>
   internal static class BuildInXmlQualifiedNames
   {
-    //#region private
-    //private const string hasComponentString = "HasComponent";
-    //private const string hasSubtypeString = "HasSubtype";
-    //private const string hasPropertyString = "HasProperty";
-    //private const string baseObjectTypeString = "BaseObjectType";
-    //private const string baseVariableTypeString = "BaseVariableType";
-    //private const string baseReferenceTypeString = "BaseReferenceType";
-    //private const string baseDataVariableType = "BaseDataVariableType";
-    //private const string baseDataType = "BaseDataType";
-    //private const string propertyType = "PropertyType";
+    #region private
+
     private const string rootFolder = "RootFolder"; // The browse name is different then the
-
-    //private const string m_HasTypeDefinition = "HasTypeDefinition";
-    //private const string hasModelParentString = "HasModelParent";
-    //private const string hasModellingRuleString = "HasModellingRule";
     private static readonly string uaNamespace = "http://opcfoundation.org/UA/";
-
     private static readonly string modelingRulePrefixString = "ModellingRule_";
-    //#endregion
+
+    #endregion private
 
     #region public
 
-    internal static XmlQualifiedName HasComponent => new XmlQualifiedName(Opc.Ua.BrowseNames.HasComponent, uaNamespace);
-    internal static XmlQualifiedName HasSubtype => new XmlQualifiedName(Opc.Ua.BrowseNames.HasSubtype, uaNamespace);
-    internal static XmlQualifiedName HasProperty => new XmlQualifiedName(Opc.Ua.BrowseNames.HasProperty, uaNamespace);
-    internal static XmlQualifiedName BaseObjectType => new XmlQualifiedName(Opc.Ua.BrowseNames.BaseObjectType, uaNamespace);
-    internal static XmlQualifiedName BaseVariableType => new XmlQualifiedName(Opc.Ua.BrowseNames.BaseVariableType, uaNamespace);
-    internal static XmlQualifiedName BaseReferenceType => new XmlQualifiedName(Opc.Ua.BrowseNames.References, uaNamespace);
-    internal static XmlQualifiedName BaseDataVariableType => new XmlQualifiedName(Opc.Ua.BrowseNames.BaseDataVariableType, uaNamespace);
-    internal static XmlQualifiedName BaseDataType => new XmlQualifiedName(Opc.Ua.BrowseNames.BaseDataType, uaNamespace);
-    internal static XmlQualifiedName PropertyType => new XmlQualifiedName(Opc.Ua.BrowseNames.PropertyType, uaNamespace);
+    internal static XmlQualifiedName HasComponent => new XmlQualifiedName(BrowseNames.HasComponent, uaNamespace);
+    internal static XmlQualifiedName HasSubtype => new XmlQualifiedName(BrowseNames.HasSubtype, uaNamespace);
+    internal static XmlQualifiedName HasProperty => new XmlQualifiedName(BrowseNames.HasProperty, uaNamespace);
+    internal static XmlQualifiedName BaseObjectType => new XmlQualifiedName(BrowseNames.BaseObjectType, uaNamespace);
+    internal static XmlQualifiedName BaseVariableType => new XmlQualifiedName(BrowseNames.BaseVariableType, uaNamespace);
+    internal static XmlQualifiedName BaseReferenceType => new XmlQualifiedName(BrowseNames.References, uaNamespace);
+    internal static XmlQualifiedName BaseDataVariableType => new XmlQualifiedName(BrowseNames.BaseDataVariableType, uaNamespace);
+    internal static XmlQualifiedName BaseDataType => new XmlQualifiedName(BrowseNames.BaseDataType, uaNamespace);
+    internal static XmlQualifiedName PropertyType => new XmlQualifiedName(BrowseNames.PropertyType, uaNamespace);
     internal static XmlQualifiedName RootFolder => new XmlQualifiedName(rootFolder, uaNamespace);
-    internal static XmlQualifiedName HasTypeDefinition => new XmlQualifiedName(Opc.Ua.BrowseNames.HasTypeDefinition, uaNamespace);
+    internal static XmlQualifiedName HasTypeDefinition => new XmlQualifiedName(BrowseNames.HasTypeDefinition, uaNamespace);
 
     //internal static XmlQualifiedName HasModelParent { get { return new XmlQualifiedName(Opc.Ua.BrowseNames.HasModelParent, uaNamespace); } }
-    internal static XmlQualifiedName HasModelingRule => new XmlQualifiedName(Opc.Ua.BrowseNames.HasModellingRule, uaNamespace);
+    internal static XmlQualifiedName HasModelingRule => new XmlQualifiedName(BrowseNames.HasModellingRule, uaNamespace);
 
     internal static XmlQualifiedName ModelingRuleSymbolicName(OpcUaModelCompiler.ModellingRule modelingRule)
     {
